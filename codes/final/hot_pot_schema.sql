b739a128dc92eaddbffa9113f83dcda8c015a567
JIA-HONG-CHU <56266563+JIA-HONG-CHU@users.noreply.github.com>
2022-01-18 14:11:08 +0800

    Update hot_pot_schema.sql

--- codes/final/hot_pot_schema.sql
+++ codes/final/hot_pot_schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE Store (
     Tel VARCHAR(12) PRIMARY KEY ,
     Addr VARCHAR(255) NOT NULL,
-	Comment_num Int,
-	Price_level Float,
+	Comment_num Int CHECK ((Comment_num>=0) or (Comment_num is NULL)),
+	Price_level Float CHECK ((Price_level>=0 and Price_level<=5) or (Price_level is NULL)),
 	Store_name VARCHAR(255),
 	Brand VARCHAR(255) Not NULL,
 	City CHAR(3) Not NULL,
 	District CHAR(3) Not NULL,
-	Avg_rating Float,
+	Avg_rating Float CHECK ((Avg_rating>=1 and Avg_rating<=5) or (Avg_rating is NULL)),
 	FOREIGN KEY (Brand) REFERENCES Hot_pot(Brand) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (City, District) REFERENCES Region(City, District) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -36,11 +36,12 @@ CREATE TABLE Commentor (
 );
 
 CREATE TABLE Operation (
-	Open_Day Int Not NULL, 
-	Begin_ Int Not NULL , 
-	End_ Int Not NULL,
+	Open_Day Int Not NULL CHECK (Open_Day>=0 and Open_Day<=6), 
+	Begin_ Int Not NULL CHECK (Begin_>=0 and Open_Day<=2400), 
+	End_ Int Not NULL CHECK (End_>=0 and Open_Day<=2400),
 	Tel VARCHAR(12),
 	PRIMARY KEY (Tel, Open_Day, Begin_, End_),
 	FOREIGN KEY(Tel) REFERENCES Store(Tel) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
 
